4a6828cd25ade774b65739d8db5ffd7205baaf39
NoReply Amazon <no-reply@amazon.com>
2025-04-09 13:48:25 +0000

    [Commit] Iteration 124 #errors = 77176 <= 77181.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Extensions/ObjectExtensions.cs][CS0308][41,19] The non-generic type 'IEnumerable' cannot be used with type arguments``.

--- src/Umbraco.Core/Extensions/ObjectExtensions.cs
+++ src/Umbraco.Core/Extensions/ObjectExtensions.cs
@@ -3,6 +3,7 @@
 
 using System.Collections;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq.Expressions;
 using System.Reflection;

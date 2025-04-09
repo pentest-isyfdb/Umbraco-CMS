4cffd69c72cfb94e0c3b6f34c07d4d794cb3a9a3
NoReply Amazon <no-reply@amazon.com>
2025-04-09 12:45:54 +0000

    [Commit] Iteration 96 #errors = 77485 <= 77493.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Routing/UrlProviderExtensions.cs][CS0246][20,30] The type or namespace name 'IEnumerable<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Routing/UrlProviderExtensions.cs
+++ src/Umbraco.Core/Routing/UrlProviderExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.DependencyInjection;

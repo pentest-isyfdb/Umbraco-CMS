3444e3ab07311dbbd7154a2fad93a7520fda06e5
NoReply Amazon <no-reply@amazon.com>
2025-04-09 15:56:44 +0000

    [Commit] Iteration 182 #errors = 76689 <= 76689.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Snippets/PartialViewSnippetCollectionBuilder.cs][CS8795][56,34] Partial method 'PartialViewSnippetCollectionBuilder.HeaderRegex()' must have an implementation part because it has accessibility modifiers.``.

--- src/Umbraco.Core/Snippets/PartialViewSnippetCollectionBuilder.cs
+++ src/Umbraco.Core/Snippets/PartialViewSnippetCollectionBuilder.cs
@@ -53,5 +53,5 @@ public partial class PartialViewSnippetCollectionBuilder : LazyCollectionBuilder
     }
 
     [GeneratedRegex("^@inherits\\s+?.*$", RegexOptions.Multiline)]
-    private static partial Regex HeaderRegex();
+    internal static partial Regex HeaderRegex();
 }

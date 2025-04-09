164b49a78b9417fc0d425e56a0227cf25008335b
NoReply Amazon <no-reply@amazon.com>
2025-04-09 13:19:13 +0000

    [Commit] Iteration 111 #errors = 77263 <= 77307.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Scoping/LockingMechanism.cs][CS0246][156,13] The type or namespace name 'HashSet<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Scoping/LockingMechanism.cs
+++ src/Umbraco.Core/Scoping/LockingMechanism.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Text;
+using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 using Umbraco.Cms.Core.Collections;
 using Umbraco.Cms.Core.DistributedLocking;

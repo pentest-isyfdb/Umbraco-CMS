d2d8f8a85a8a2a33f3eb7c46a87d2b03edd72b89
NoReply Amazon <no-reply@amazon.com>
2025-04-09 12:34:48 +0000

    [Commit] Iteration 91 #errors = 77524 <= 77539.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Extensions/StringExtensions.cs][CS0246][178,55] The type or namespace name 'HashSet<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Extensions/StringExtensions.cs
+++ src/Umbraco.Core/Extensions/StringExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Umbraco.
 // See LICENSE for more details.
 
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;

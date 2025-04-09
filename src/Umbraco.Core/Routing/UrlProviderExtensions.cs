6eb1523293f92ccd6058f151426cb113a69e9a60
NoReply Amazon <no-reply@amazon.com>
2025-04-09 12:43:38 +0000

    [Commit] Iteration 95 #errors = 77493 <= 77497.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Routing/UrlProviderExtensions.cs][CS0246][19,25] The type or namespace name 'Task<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Routing/UrlProviderExtensions.cs
+++ src/Umbraco.Core/Routing/UrlProviderExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Threading.Tasks;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Core.Models;

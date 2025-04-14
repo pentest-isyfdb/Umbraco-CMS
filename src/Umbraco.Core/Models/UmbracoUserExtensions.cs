da40390af2f5787bcaf41ba79a50772796c53e70
NoReply Amazon <no-reply@amazon.com>
2025-04-14 20:51:00 +0000

    [Commit] Iteration 21 #errors = 79587 <= 79588.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Models/UmbracoUserExtensions.cs][CS0246][14,19] The type or namespace name 'IEnumerable<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Models/UmbracoUserExtensions.cs
+++ src/Umbraco.Core/Models/UmbracoUserExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Umbraco.
 // See LICENSE for more details.
 
+using System.Collections.Generic;
 using System.Globalization;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Configuration.Models;

c542718d84ca2c3f097f5d33c754b2f42fc38194
NoReply Amazon <no-reply@amazon.com>
2025-04-14 21:27:07 +0000

    [Commit] Iteration 37 #errors = 79521 <= 79524.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Extensions/TypeLoaderExtensions.cs][CS0246][16,19] The type or namespace name 'IEnumerable<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Extensions/TypeLoaderExtensions.cs
+++ src/Umbraco.Core/Extensions/TypeLoaderExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Umbraco.
 // See LICENSE for more details.
 
+using System.Collections.Generic;
 using Umbraco.Cms.Core.Actions;
 using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Composing;

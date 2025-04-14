e3c6ea5c1cba98d9eb1333198792b0b45e71a793
NoReply Amazon <no-reply@amazon.com>
2025-04-14 22:58:31 +0000

    [Commit] Iteration 78 #errors = 79288 <= 79296.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Media/EmbedProviders/OEmbedProviderBase.cs][CS0246][26,21] The type or namespace name 'Task<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Media/EmbedProviders/OEmbedProviderBase.cs
+++ src/Umbraco.Core/Media/EmbedProviders/OEmbedProviderBase.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Xml;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 using Umbraco.Cms.Core.Serialization;
 
 namespace Umbraco.Cms.Core.Media.EmbedProviders;

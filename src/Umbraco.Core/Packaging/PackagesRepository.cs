49b4fd0d472654f20dcfa1a92338af20168aa74e
NoReply Amazon <no-reply@amazon.com>
2025-04-15 02:06:36 +0000

    [Commit] Iteration 163 #errors = 78384 <= 78409.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Packaging/PackagesRepository.cs][CS0246][689,32] The type or namespace name 'Stream' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Packaging/PackagesRepository.cs
+++ src/Umbraco.Core/Packaging/PackagesRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
+using System.IO;
 using System.IO.Compression;
 using System.Xml.Linq;
 using Microsoft.Extensions.Options;

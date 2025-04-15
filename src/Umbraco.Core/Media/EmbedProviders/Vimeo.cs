0e5eedb5bcce4d63ba7ac11468eebb0a488ab1fd
NoReply Amazon <no-reply@amazon.com>
2025-04-15 01:57:38 +0000

    [Commit] Iteration 159 #errors = 78422 <= 78428.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Media/EmbedProviders/Vimeo.cs][CS0246][28,99] The type or namespace name 'CancellationToken' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Media/EmbedProviders/Vimeo.cs
+++ src/Umbraco.Core/Media/EmbedProviders/Vimeo.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Xml;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Umbraco.Cms.Core.Serialization;
 
 namespace Umbraco.Cms.Core.Media.EmbedProviders;

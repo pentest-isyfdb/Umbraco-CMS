871cd2d692f760220cfd7cf74dda40afb482a290
NoReply Amazon <no-reply@amazon.com>
2025-04-15 02:55:43 +0000

    [Commit] Iteration 185 #errors = 78301 <= 78308.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Services/AuditService.cs][CS0246][90,12] The type or namespace name 'IEnumerable<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Services/AuditService.cs
+++ src/Umbraco.Core/Services/AuditService.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Models;

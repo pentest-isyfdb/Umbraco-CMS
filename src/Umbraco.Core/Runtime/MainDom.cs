f227bda1d0fffbc415198ff79cd3fc169c626551
NoReply Amazon <no-reply@amazon.com>
2025-04-15 07:13:56 +0000

    [Commit] Iteration 302 #errors = 78092 <= 78096.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Runtime/MainDom.cs][CS0246][39,17] The type or namespace name 'Task' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Runtime/MainDom.cs
+++ src/Umbraco.Core/Runtime/MainDom.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Umbraco.Cms.Core.Hosting;
 using Umbraco.Extensions;

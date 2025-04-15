ebce7a1963ed47dfe98f75ebf6166d8963453ee3
NoReply Amazon <no-reply@amazon.com>
2025-04-15 00:41:36 +0000

    [Commit] Iteration 125 #errors = 78721 <= 78727.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Media/EmbedProviders/Flickr.cs][CS0246][29,99] The type or namespace name 'CancellationToken' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Media/EmbedProviders/Flickr.cs
+++ src/Umbraco.Core/Media/EmbedProviders/Flickr.cs
@@ -2,6 +2,8 @@ using System;
 using System.Net;
 using System.Xml;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Umbraco.Cms.Core.Serialization;
 
 namespace Umbraco.Cms.Core.Media.EmbedProviders;

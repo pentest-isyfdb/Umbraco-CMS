19913ee24f154293a2f83af7948fbf12509c2049
NoReply Amazon <no-reply@amazon.com>
2025-04-15 01:14:59 +0000

    [Commit] Iteration 140 #errors = 78612 <= 78614.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Extensions/DictionaryExtensions.cs][CS0246][307,22] The type or namespace name 'Task<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Extensions/DictionaryExtensions.cs
+++ src/Umbraco.Core/Extensions/DictionaryExtensions.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 using Umbraco.Cms.Core;
 
 namespace Umbraco.Extensions;

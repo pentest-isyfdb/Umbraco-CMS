67e0af883cc4e6aa3d70519e1eeec9e9a4139a6b
NoReply Amazon <no-reply@amazon.com>
2025-04-15 00:32:38 +0000

    [Commit] Iteration 121 #errors = 78733 <= 78732.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Media/EmbedProviders/Flickr.cs][CS0307][21,21] The namespace 'Umbraco.Cms.Core.Dictionary' cannot be used with type arguments``.

--- src/Umbraco.Core/Media/EmbedProviders/Flickr.cs
+++ src/Umbraco.Core/Media/EmbedProviders/Flickr.cs
@@ -18,7 +18,7 @@ public class Flickr : OEmbedProviderBase
 
     public override string[] UrlSchemeRegex => new[] { @"flickr.com\/photos\/*", @"flic.kr\/p\/*" };
 
-    public override Dictionary<string, string> RequestParams => new();
+    public override Dictionary<string, string> RequestParams => new Dictionary<string, string>();
 
     [Obsolete("Use GetMarkupAsync instead. This will be removed in Umbraco 15.")]
     public override string? GetMarkup(string url, int maxWidth = 0, int maxHeight = 0)

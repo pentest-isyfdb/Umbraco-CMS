12ef70eff2050c6a3b5cab2a88f51e305009162c
NoReply Amazon <no-reply@amazon.com>
2025-04-15 04:26:27 +0000

    [Commit] Iteration 226 #errors = 78696 <= 78702.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Media/EmbedProviders/Hulu.cs][CS0246][27,99] The type or namespace name 'CancellationToken' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Media/EmbedProviders/Hulu.cs
+++ src/Umbraco.Core/Media/EmbedProviders/Hulu.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Umbraco.Cms.Core.Serialization;
 
 namespace Umbraco.Cms.Core.Media.EmbedProviders;

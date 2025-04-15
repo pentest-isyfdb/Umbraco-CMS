d721f3462767e52e92c1e4825c08d52c470f566b
NoReply Amazon <no-reply@amazon.com>
2025-04-15 02:28:51 +0000

    [Commit] Iteration 173 #errors = 78357 <= 78359.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Events/INotificationAsyncHandler.cs][CS0246][35,28] The type or namespace name 'IEnumerable<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Events/INotificationAsyncHandler.cs
+++ src/Umbraco.Core/Events/INotificationAsyncHandler.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Umbraco.
 // See LICENSE for more details.
 
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Umbraco.Cms.Core.Notifications;

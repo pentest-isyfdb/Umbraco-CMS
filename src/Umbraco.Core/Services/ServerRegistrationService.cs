ee59b558f13b1d7df15d9db04c8866201b49c778
NoReply Amazon <no-reply@amazon.com>
2025-04-15 06:18:51 +0000

    [Commit] Iteration 277 #errors = 78231 <= 78234.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Services/ServerRegistrationService.cs][CS0246][142,12] The type or namespace name 'IEnumerable<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Services/ServerRegistrationService.cs
+++ src/Umbraco.Core/Services/ServerRegistrationService.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Hosting;
 using Umbraco.Cms.Core.Models;

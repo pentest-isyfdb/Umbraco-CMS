6e977d78ff6c4dbf4da48a58abe1e2fd2ca8a303
NoReply Amazon <no-reply@amazon.com>
2025-04-15 06:07:51 +0000

    [Commit] Iteration 272 #errors = 78364 <= 78365.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Services/IDictionaryPermissionService.cs][CS0246][10,5] The type or namespace name 'Task<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- src/Umbraco.Core/Services/IDictionaryPermissionService.cs
+++ src/Umbraco.Core/Services/IDictionaryPermissionService.cs
@@ -1,6 +1,7 @@
 using Umbraco.Cms.Core.Models.Membership;
 using Umbraco.Cms.Core.Services.AuthorizationStatus;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Umbraco.Cms.Core.Services;
 

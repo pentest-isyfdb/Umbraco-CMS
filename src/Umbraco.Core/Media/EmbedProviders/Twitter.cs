e362f54ec08ef2e600a9b088b80e372fc60486e0
NoReply Amazon <no-reply@amazon.com>
2025-04-15 04:06:29 +0000

    [Commit] Iteration 217 #errors = 78720 <= 78721.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Core/Media/EmbedProviders/Twitter.cs][CS0307][21,21] The namespace 'Umbraco.Cms.Core.Dictionary' cannot be used with type arguments``.

--- src/Umbraco.Core/Media/EmbedProviders/Twitter.cs
+++ src/Umbraco.Core/Media/EmbedProviders/Twitter.cs
@@ -18,7 +18,7 @@ public class Twitter : OEmbedProviderBase
 
     public override string[] UrlSchemeRegex => new[] { @"twitter.com/.*/status/.*" };
 
-    public override Dictionary<string, string> RequestParams => new();
+    public override System.Collections.Generic.Dictionary<string, string> RequestParams => new();
 
     [Obsolete("Use GetMarkupAsync instead. This will be removed in Umbraco 15.")]
     public override string? GetMarkup(string url, int maxWidth = 0, int maxHeight = 0)
